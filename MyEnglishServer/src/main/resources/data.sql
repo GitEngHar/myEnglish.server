96b41352c8d99588ea721367f59c926df3c2ad4e
GitEngHar <harukii1999@gmail.com>
2024-09-29 15:50:34 +0900

    MYSQLへの移行とdetails RESTAPIに削除と編集を追加

--- MyEnglishServer/src/main/resources/data.sql
+++ MyEnglishServer/src/main/resources/data.sql
@@ -1,15 +1,20 @@
-INSERT INTO user_root (name,created_date,update_date) 
-VALUES
-('testuser',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+DELETE FROM question_details_answer_plugin;
+DELETE FROM question_details_plugin;
+DELETE FROM question_core;
+DELETE FROM user_root;
 
-INSERT INTO question_core (owner_user_id,question_title,created_date,update_date) 
+INSERT IGNORE INTO user_root (user_id,name,created_date,update_date)
 VALUES
-(1,'testTitle' , CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(1,'testuser',NOW(), NOW());
 
-INSERT INTO question_details_plugin (question_title_id,question_word,created_date,update_date) 
+INSERT IGNORE INTO question_core (question_title_id,owner_user_id,question_title,created_date,update_date)
 VALUES
-(1,'道の隅に黄色い花が咲いている',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(1,1,'testTitle' , NOW(), NOW());
 
-INSERT INTO question_details_answer_plugin (question_title_id,question_details_id,answer_id,answer_candidate_no_1,answer_candidate_no_2,answer_candidate_no_3,answer_candidate_no_4,created_date,update_date) 
+INSERT IGNORE INTO question_details_plugin (question_details_id,question_title_id,question_word,created_date,update_date)
 VALUES
-(1,1,1,'Yellow flowers in the corner of the road.','hogehoge','hogehoge','hogehoge',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+(1,1,'道の隅に黄色い花が咲いている',NOW(), NOW());
+
+INSERT IGNORE INTO question_details_answer_plugin (question_answer_id,question_title_id,question_details_id,answer_id,answer_candidate_no_1,answer_candidate_no_2,answer_candidate_no_3,answer_candidate_no_4,created_date,update_date)
+VALUES
+(1,1,1,1,'Yellow flowers in the corner of the road.','hogehoge','hogehoge','hogehoge',NOW(), NOW());
