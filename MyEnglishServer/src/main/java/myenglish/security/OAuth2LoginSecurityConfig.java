5f6f73260e6edf1e9437587c13d1558878be0104
GitEngHar <harukii1999@gmail.com>
2024-11-02 16:39:52 +0900

    oauth認証を実装する2

--- MyEnglishServer/src/main/java/myenglish/security/OAuth2LoginSecurityConfig.java
+++ MyEnglishServer/src/main/java/myenglish/security/OAuth2LoginSecurityConfig.java
@@ -15,14 +15,15 @@ public class OAuth2LoginSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login/**").permitAll() // login配下は認証不要
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(withDefaults())
-
+                .csrf(csrf -> csrf.disable()) // CSRFを無効化
+                .oauth2Login(withDefaults())  // 認証設定にデフォルトの設定を利用する (applicatin.yml)
                 .oauth2Login(customizer -> customizer
-                        .defaultSuccessUrl("/loginsuccess",true));
-
+                        .defaultSuccessUrl("/loginsuccess",true)); // ログイン成功後にログイン成功リダイレクト先に遷移。trueでいつでも有効にする。
 
         return http.build();
     }
+
 }
