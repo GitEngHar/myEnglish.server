9728a234a2384521c8de772170bc41bdfb25807a
GitEngHar <harukii1999@gmail.com>
2024-12-14 20:33:00 +0900

    Session機能がちゃんと実装できるまで封じる

--- MyEnglishServer/src/main/java/myenglish/session/SessionConfig.java
+++ MyEnglishServer/src/main/java/myenglish/session/SessionConfig.java
@@ -1,9 +1,14 @@
 package myenglish.session;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
+//RedisReactiveSessionRepository
 
 @Configuration
-@EnableRedisWebSession()
+//@EnableRedisWebSession()
 public class SessionConfig {
 }
